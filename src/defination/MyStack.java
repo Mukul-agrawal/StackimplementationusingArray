7cd21d3db2b08dad965ae484293f8ae01669a32f
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-14 19:53:23 +0530

    create data field :array,top and maxSize

--- src/defination/MyStack.java
+++ src/defination/MyStack.java
@@ -3,6 +3,10 @@ package defination;
 import adt.StackADT;
 
 public class MyStack<E> implements StackADT<E> {
+    public E[] array;
+    public int top;
+    public int maxSize;
+
     @Override
     public void push(E item) {
 
